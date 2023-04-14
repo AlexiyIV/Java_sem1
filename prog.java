ea9565f78de84dce5eb9ae1ca46d23c20bd21c52
Сучков Алексей <AlexiySV@yandex.ru>
2023-04-14 11:32:25 +0300

    task1

---
+++ prog.java
@@ -0,0 +1,10 @@
+package Java_sem1;
+
+
+import Java_sem1.task.*;
+
+public class prog {
+    public static void main(String[] args) {
+        task1.run();
+    }
+}
---
+++ task/task2.java
@@ -0,0 +1,4 @@
+package Java_sem1.task;
+
+public class task2 {
+}
